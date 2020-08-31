b6004662eff23e55cd930b17966c96023ce0884e
Jone Bulande <JoneBulande@gmail.com>
2020-08-31 12:32:31 +0200

    adicionei o botão scroll up automatico

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+const ceta = document.querySelector('.ceta');
+const btn = document.querySelector('.btn');
+const grid = document.querySelector('.grid');
+
 function initScrollSuave() {
 	const links = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
 
@@ -12,35 +16,50 @@ function initScrollSuave() {
 	}
 
 	links.forEach((link)=>{
-		link.addEventListener("click", scrollSuave)
+		link.addEventListener("click", scrollSuave);
 	});
+	
+	ceta.addEventListener("click", scrollSuave);
+	btn.addEventListener("click", scrollSuave);
 }
 initScrollSuave();
 
 
-const downs = document.querySelectorAll('[data-anime="down"]');
-const ups = document.querySelectorAll('[data-anime="up"]');
+function initAnimacaoScroll() {
+	
+	const downs = document.querySelectorAll('[data-anime="down"]');
+	const ups = document.querySelectorAll('[data-anime="up"]');
 
-const windowMetade = window.innerHeight * 0.8;
 
-function animationScroll(){
-	downs.forEach((down)=>{
-		const top = down.getBoundingClientRect().top;
-		const isVisible = (top - windowMetade) < 0;
-		if (isVisible) {
-			down.classList.add("ativo");
+	const windowMetade = window.innerHeight * 0.8;
+
+	function animationScroll(){
+		
+		if (grid.classList.contains('ativo')) {
+			ceta.classList.add('ativo');
 		}else{
-			down.classList.remove("ativo");
+			ceta.classList.remove('ativo');
 		}
-	});
-	ups.forEach((up)=>{
-		const top = up.getBoundingClientRect().top;
-		const isVisible = (top - windowMetade) < 0;
-		if (isVisible) {
-			up.classList.add("ativo");
-		}else{
-			up.classList.remove("ativo");
-		}
-	});
+
+		downs.forEach((down)=>{
+			const top = down.getBoundingClientRect().top;
+			const isVisible = (top - windowMetade) < 0;
+			if (isVisible) {
+				down.classList.add("ativo");
+			}else{
+				down.classList.remove("ativo");
+			}
+		});
+		ups.forEach((up)=>{
+			const top = up.getBoundingClientRect().top;
+			const isVisible = (top - windowMetade) < 0;
+			if (isVisible) {
+				up.classList.add("ativo");
+			}else{
+				up.classList.remove("ativo");
+			}
+		});
+	}
+	window.addEventListener("scroll", animationScroll);
 }
-window.addEventListener("scroll", animationScroll)
+initAnimacaoScroll();
